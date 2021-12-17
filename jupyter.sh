c5311fc909e3de8132fa566d2f5cf5a51c268d46
remomomo <remomomonti@gmail.com>
2021-12-17 10:57:55 +0100

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -3,8 +3,8 @@
 #SBATCH --output=jupyter_%j.out
 #SBATCH --partition=hpcpu,vcpu # -p
 #SBATCH --cpus-per-task=1 # -c
-#SBATCH --mem=16gb
-#SBATCH --time=12:00:00 # 5 minutes  
+#SBATCH --mem=6gb
+#SBATCH --time=1:00:00 # 5 minutes  
  
 # Initialize conda:
 eval "$(conda shell.bash hook)"
@@ -19,4 +19,3 @@ fi
 conda activate base
 
 jupyter-lab --port=$port
-i
